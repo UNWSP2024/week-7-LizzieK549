e2bb2dc9469466c92d7059e3b7f7023873f484f8
LizzieK549 <klizzy630@gmail.com>
2024-10-18 21:47:20 -0500

    Week 7 Assignment
    Project 7 Elizabeth Kerr 10/18/24

--- program_3.py
+++ program_3.py
@@ -1,25 +1,45 @@
-# Program #3: US_Population
+#Have the user input (using a loop) various
+#information that contains three pieces of data:
+        #year, name of state, and population.
+#Store all of this information in a list
+#of lists. For example it might be stored like this:'''
+
+
 def main():
-    # Have the user input (using a loop) various information that contains three pieces of data: 
-    # year, name of state, and population.  
-    # Store all of this information in a list of lists.  For example it might be stored like this:
-    
-    # [[2010, "Maine", 1987435], [2010,"Minnesota",6873202], [2011, "Iowa", 3421988]]
     all_entered_values = []
 
-    # Now have the user enter a year. 
-    
+    while True:
+        year = input("Enter the year or 'STOP' to stop: ")
+        if year.upper() == 'STOP':
+            break
+        state = input("Enter state name: ")
+        population = int(input("Enter the population: "))
+        all_entered_values.append([int(year), state, population])
+
+        # [ [2010, "Maine", 1987435], [2010, "Minnesota", 6873202], [2011, "Iowa",3421988]]
+        # all entered values = []
+
+        # Now have the user enter a year.
+    year_to_sum = int(input("Enter a specific year you would like information for: "))
+
+    sum_population_for_year(all_entered_values, year_to_sum)
     # The program will add the populations from all states in the list of list for that year only.
-    # Pass the list and year to the sum_population_for_year
+    # Pass the list and year to the
+
 
 def sum_population_for_year(all_entered_values, year_to_sum):
-    # Loop through and sum the populations for the appropriate year. 
-    # e.g. for the list on line 7 the total would be 8,860,637 if the user enterd 2010 for the year to sum,
-    # or 3,421,988 if they enterd 2011 for the year to sum.
+    totalPopulation = 0
+    for entry in all_entered_values:
+        year, state, population = entry
+        if year == year_to_sum:
+            totalPopulation += population
+    print("Total population for the year is", totalPopulation)
 
-    # print the totalled population
+    # Loop through and sum the populations for the appropriate year.
 
 
-# Call the main function.
+# e.g. for the list on line 7 the total would be 8,860,637 if the user enterd 2010 for the year to sum,
+# or 3,421,988 if they enterd 2011 for the year to sum.
+
 if __name__ == '__main__':
     main()
--- program_4.py
+++ program_4.py
@@ -1,10 +1,57 @@
 # Program #4: Coordinates
-# Write a distance function that will take two 3-dimensional coordinates (as input) 
-# and will return (as output) the distance between those points in space.  
-# The 3-dimensional coordinates must be stored as tuples.
+import math
 
-# Now write a mainline that has the user enter the two tuples.  
-# The mainline calls the distance function and stores the distance in a variable.  The mainline then displays the distance.  
-# Also include exception handling to deal with faulty input.
-# The distance between two points (x1,y1,z1) and (x2, y2, z2) is 
-#    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2) 
+def coordinates():
+    coordinate1 = x1, x2, x3
+    coordinate2  = y1, y2, y3
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+
+
+#Program #4: Coordinates
+
+#Write a distance function that will take two 3-dimensional
+#coordinates (as input) and will return (as output) the distance
+#between those points in space. The 3-dimensional coordinates must be
+#stored as tuples.
+#The mainline calls the distance function and stores the
+#distance in a variable. The mainline then displays the distance.
+# Also include exception handling to deal
+#with faulty input.
+# The distance between two points (x1,y1,z1)
+#and (x2, y2, z2) is
+#given by:
+
+#sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 -z2)^2)'''
+
+import math
+
+def coordinates(coordinate1, coordinate2) :   #remember to take both positional arguments here
+    x1, y1, z1 = coordinate1
+    x2, y2, z2 = coordinate2
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2) #math for formula
+
+def main():
+    try:
+        #Now write a mainline that has the user enter the two tuples.
+        x1 = float(input("Enter the X1 for the first coordinate: "))
+        y1 = float(input("Enter the Y1 for the first coordinate: "))
+        z1 = float(input("Enter the Z1 for the first coordinate: "))
+
+        x2 = float(input("Enter the X2 for the second coordinate: "))
+        y2 = float(input("Enter the Y2 for the second coordinate: "))
+        z2 = float(input("Enter the Z2 for the second coordinate: "))
+
+        coordinate1 = (x1, y1, z1)  #stores distance in a variable
+        coordinate2 = (x2, y2, z2)
+
+        answer = coordinates(coordinate1, coordinate2)
+        print("The distance between the 3D coordinates is ", answer)
+
+    except ValueError:    #deals with non-number inputs
+        print("Invalid input. Place enter numbers")
+
+    except Exception as e:  #handles all other errors
+        print("Error, please try again.😢")
+
+if __name__ == '__main__':
+    main()
